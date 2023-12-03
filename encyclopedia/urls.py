2785aeb287b8050071396937b202c8a0adf03364
Marshall <marshallpowell13@gmail.com>
2023-12-03 12:26:15 +0000

    cs50w wiki prod - added markdown2 pages work

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import content_page
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("/wiki/<str:title>/", views.entry_detail, name='entry_detail'),
+    path('content/<str:filename>/', content_page, name='content_page'),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .util import get_entry
+import markdown2
+import os
 from . import util
 
 
@@ -8,3 +10,22 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry_detail(request, title):
+    entry_content = get_entry(title)
+    return render(request, 'entry_detail.html', {'title': title, 'content': entry_content})
+''' def pages():
+        util.get_entry(title)
+    '''
+
+def content_page(request,filename):
+    file_path = os.path.join('entries', filename + '.md')
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+            html_content = markdown2.markdown(content)
+    except FileNotFoundError:
+        html_content = "<p>File Not Found.</p>"
+
+    return render(request, 'encyclopedia/contents.html', {'html_content': html_content})
+        
